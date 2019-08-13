cdb642d2544df9bb9b5b827b7d1a66c1c6d8ef6a
c1dKiv <1020789321@qq.com>
2019-08-13 23:40:42 +0800

    提交本地文件

--- client/client/gamesrc/network/main.cpp
+++ client/client/gamesrc/network/main.cpp
@@ -1,7 +1,11 @@
+#include <WinSock2.h>
+#include <windows.h>
 #include <iostream>
 using namespace std;
 
 void main()
 {
+	auto socketTCP = socket(AF_INET, SOCK_STREAM, 0);
 	cout << "walkout_client" << endl;
+	shutdown(socketTCP, SD_BOTH);
 }
